aafa393f1ba01a39f5b38aa69d03bc52e0b3bf0d
sdaawr <45720212+sdaawr@users.noreply.github.com>
2019-06-30 07:27:13 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,36 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("594491787842945055")
+let channel =     client.channels.get("594895007744393259")
 setInterval(function() {
-channel.send(`I Need credits Hh bm hhbm hhbm hhbm cretors by devil I Need credits Hh bm hhbm hhbm hhbm cretors by devil`);
+channel.send(`Yes I do, I believe
+That one day I will be, where I was
+Right there, right next to you
+And it's hard, the days just seem so dark
+The moon, and the stars, are nothing without you
+Your touch, your skin, where do I begin?
+No words can explain, the way i'm missing you
+Deny this emptiness, this hole that i'm inside
+These tears, they tell their own story
+You told me not to cry when you were gone
+But the feeling's overwhelming, it's much too strong
+Can I lay by your side, next to you, you
+And make sure you're alright
+I'll take care of you,
+And I don't want to be here if I can't be with you tonight
+I'm reaching out to you
+Can you hear my call 
+This hurt that I've been through
+I'm missing you, missing you like crazy
+Can I lay by your side, next to you, to you
+And make sure you're alright
+I'll take care of you,
+And I don't want to be here if I can't be with you tonight
+Lay me down tonight, lay me by your side
+Lay me down tonight
+Lay me by your side
+Can I lay by your side, next to you, you
+`);
 }, 30)
 })
 
